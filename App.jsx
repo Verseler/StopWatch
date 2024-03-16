60d658c3aa02a6dac94850fec68e1bce0d2468ba
Verseler <handuman.verselerkerr@gmail.com>
2024-03-16 23:03:28 +0800

    feat: stopwatch functionality with logo

---
+++ App.jsx
@@ -0,0 +1,75 @@
+import {useRef, useState} from 'react';
+import {StyleSheet, View, SafeAreaView, StatusBar} from 'react-native';
+
+import ControlButtons from './components/ControlButtons';
+import Timer from './components/Timer';
+
+export default function App() {
+  const [time, setTime] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const [isPaused, setIsPaused] = useState(true);
+  const intervalRef = useRef(null);
+  const startTimeRef = useRef(0);
+
+  // Function to start the stopwatch
+  const startStopwatch = () => {
+    startTimeRef.current = Date.now() - time * 1000;
+    intervalRef.current = setInterval(() => {
+      setTime(Math.floor((Date.now() - startTimeRef.current) / 1000));
+    }, 1000);
+    setIsActive(true);
+    setIsPaused(false);
+  };
+  // Function to pause the stopwatch
+  const pauseStopwatch = () => {
+    clearInterval(intervalRef.current);
+    setIsPaused(true);
+  };
+  // Function to reset the stopwatch
+  const resetStopwatch = () => {
+    clearInterval(intervalRef.current);
+    setTime(0);
+    setIsActive(false);
+  };
+  // Function to resume the stopwatch
+  const resumeStopwatch = () => {
+    startTimeRef.current = Date.now() - time * 1000;
+
+    intervalRef.current = setInterval(() => {
+      setTime(Math.floor((Date.now() - startTimeRef.current) / 1000));
+    }, 1000);
+
+    setIsActive(true);
+    setIsPaused(false);
+  };
+  return (
+    <SafeAreaView style={styles.safeAreaContainer}>
+      <StatusBar
+        backgroundColor="#121212"
+      />
+    <View style={styles.container}>
+      <Timer time={time} />
+      <ControlButtons
+        isActive={isActive}
+        isPaused={isPaused}
+        pauseStopwatch={pauseStopwatch}
+        resetStopwatch={resetStopwatch}
+        resumeStopwatch={resumeStopwatch}
+        startStopwatch={startStopwatch}
+      />
+    </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeAreaContainer: {
+    backgroundColor: 'red',
+  },
+  container: {
+    height: '100%',
+    backgroundColor: '#121212',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ components/Timer.jsx
@@ -0,0 +1,31 @@
+import {StyleSheet, Text, View} from 'react-native';
+
+export default function Timer({time}) {
+  const minutes = Math.floor(time / 60)
+    .toString()
+    .padStart(2, '0');
+  const seconds = (time % 60).toString().padStart(2, '0');
+
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.digits}>{minutes}:</Text>
+      </View>
+
+      <View>
+        <Text style={styles.digits}>{seconds}</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+  },
+  digits: {
+    fontSize: 70,
+    marginBottom: 30,
+    color: '#fff',
+  },
+});
